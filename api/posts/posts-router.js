42973fa593f90f4e5e8e42678f6d4a4ae3836e43
“Mariana <marysiq@hotmail.com>
2024-09-06 16:37:07 -0700

    wip

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -3,4 +3,24 @@ const express = require('express')
 
 const router = express.Router()
 
+
+router.get('/', (req, res) => {
+    res.json('foo')
+})
+router.get('/', (req, res) => {
+    
+})
+router.post('/', (req, res) => {
+    
+})
+router.put('/', (req, res) => {
+    
+})
+router.delete('/', (req, res) => {
+    
+})
+router.get('/', (req, res) => {
+    
+})
+
 module.exports = router
